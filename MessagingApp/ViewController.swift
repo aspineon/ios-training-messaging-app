168c22f4d24d66fa5cf4de4a7559c754c0f3bcff
landrzejewski <landrzejewski.poczta@gmail.com>
2020-06-03 09:17:11 +0200

    Initial Commit

---
+++ MessagingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessagingApp
+//
+//  Created by Łukasz Andrzejewski on 03/06/2020.
+//  Copyright © 2020 Inbright Łukasz Andrzejewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
